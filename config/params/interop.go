422ed7abab777d010fe3541aed802999ad851028
nirmal_gupta <nirmalk.gupta@gmail.com>
2025-06-03 17:12:07 +0200

    changed for create sync

--- config/params/interop.go
+++ config/params/interop.go
@@ -3,14 +3,21 @@ package params
 // InteropConfig provides a generic config suitable for interop testing.
 func InteropConfig() *BeaconChainConfig {
 	c := MainnetConfig().Copy()
+// fixme: gluon : change start
+    // Custom fork versions
+    c.ConfigName = InteropName
+    c.GenesisForkVersion = []byte{0x20, 0x00, 0x00, 0x89}
+    c.AltairForkVersion = []byte{0x20, 0x00, 0x00, 0x90}
+    c.BellatrixForkVersion = []byte{0x20, 0x00, 0x00, 0x91}
+    c.CapellaForkVersion = []byte{0x20, 0x00, 0x00, 0x92}
+    c.DenebForkVersion = []byte{0x20, 0x00, 0x00, 0x93}
 
-	// Prysm constants.
-	c.ConfigName = InteropName
-	c.GenesisForkVersion = []byte{0, 0, 0, 235}
-	c.AltairForkVersion = []byte{1, 0, 0, 235}
-	c.BellatrixForkVersion = []byte{2, 0, 0, 235}
-	c.CapellaForkVersion = []byte{3, 0, 0, 235}
-	c.DenebForkVersion = []byte{4, 0, 0, 235}
+	c.GenesisEpoch =         0
+	c.AltairForkEpoch =      0
+	c.BellatrixForkEpoch =   0
+	c.CapellaForkEpoch =     0
+	c.DenebForkEpoch =       18446744073709551615
+// fixme: gluon : change end
 
 	c.InitializeForkSchedule()
 	return c
